17957dc4c46ff9e5e36983ee0d5711c11820adfd
wkwyatt <wkwyatt1@gmail.com>
2015-11-17 11:13:32 -0500

    syntax fix changed var name

--- controller.js
+++ controller.js
@@ -1,6 +1,6 @@
 var countryApp = angular.module('countryApp', []);
-myApp.controller('countryCtrl',function($scope, $http) {
-	$http.get('countries.jsosn').success(function(countryData) {
+countryApp.controller('countryCtrl',function($scope, $http) {
+	$http.get('countries.json').success(function(countryData) {
 		$scope.countries = countryData;
 	});
 });
